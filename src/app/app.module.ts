c296b1ae435d11df20f7b360c27d7af3ea6a3491
Amit775 <38419204+Amit775@users.noreply.github.com>
2021-12-08 18:52:36 +0200

    change views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ISmokeComponent } from './features/i-smoke/i-smoke.component';
 import { SmokesHistoryComponent } from './features/smokes-history/smokes-history.component';
 import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
-import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { environment } from '../environments/environment';
+import { TopBarComponent } from './layout/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ISmokeComponent,
-    SmokesHistoryComponent
+    SmokesHistoryComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     MatButtonModule,
     MatProgressSpinnerModule,
-    environment.production ? [] : AkitaNgDevtools.forRoot(),
-    AkitaNgRouterStoreModule
+    MatTabsModule,
+    MatIconModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot()
   ],
   providers: [{ provide: NG_ENTITY_SERVICE_CONFIG, useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' }}],
   bootstrap: [AppComponent]
---
+++ src/app/layout/top-bar/top-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { Route } from '@angular/router';
+
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.scss']
+})
+export class TopBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
