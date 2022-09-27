8e5e663a96dbdae32b9edae0288700d08993ebbe
Amit775 <38419204+Amit775@users.noreply.github.com>
2022-09-27 15:11:53 +0300

    better perfoemance for calendar badge

--- src/app/features/smokes-history/smokes-history.component.ts
+++ src/app/features/smokes-history/smokes-history.component.ts
@@ -8,9 +8,11 @@ import { ISmoke } from 'src/app/core/smokes/smokes.store';
 import { default as flatpickr } from 'flatpickr';
 import { DayElement, Instance } from 'flatpickr/dist/types/instance';
 import { EntityAction, EntityActions } from '@datorama/akita';
+import { Hook } from 'flatpickr/dist/types/options';
 
 type Indexable<T> = {
-	[index: number]: T
+	[index: number]: T;
+	length: number;
 }
 
 @Component({
@@ -25,6 +27,8 @@ export class SmokesHistoryComponent implements OnInit, AfterViewInit {
 	@ViewChild('container') private container!: ElementRef;
 	instance!: Instance;
 
+	daysIndex: Record<number, number> = {}
+
 	constructor(
 		private smokes: SmokesQuery,
 		private service: SmokesService,
@@ -38,7 +42,13 @@ export class SmokesHistoryComponent implements OnInit, AfterViewInit {
 	ngAfterViewInit(): void {
 		this.instance = flatpickr(this.container.nativeElement, {
 			inline: true,
-			onDayCreate: (a, b, c, dayElement) => this.addCountBadge(dayElement)
+			onDayCreate: (a, b, c, dayElement) => this.addCountBadge(dayElement),
+			onReady: [
+				this.indexByDateHook()
+			],
+			onMonthChange: [
+				this.indexByDateHook()
+			]
 		});
 		(window as any).fp = this.instance;
 		this.smokes.selectEntityAction([EntityActions.Add, EntityActions.Remove]).pipe(
@@ -51,15 +61,31 @@ export class SmokesHistoryComponent implements OnInit, AfterViewInit {
 
 	getDayElement(date: Date): DayElement | null {
 		const nodes = this.instance.days.childNodes;
-		const index = this.binarySearch(nodes, date, 0, nodes.length, (node) => (node as DayElement).dateObj, (date) => date.valueOf());
+		const index = this.daysIndex[date.valueOf()];
+		// const index = this.binarySearch(nodes, date, 0, nodes.length, (node) => (node as DayElement).dateObj, (date) => date.valueOf());
 
 		if (index < 0) return null
 		return nodes[index] as DayElement;
 	}
 
+	indexByDateHook(): Hook {
+		return (a, b, instance) => {
+			this.daysIndex = this.indexByDate(instance.days.childNodes);
+		};
+	}
+
+	indexByDate(arr: Indexable<ChildNode>): Record<number, number> {
+		const result: Record<number, number> = {};
+		for (let index = 0; index < arr.length; index++) {
+			result[(arr[index] as DayElement).dateObj.valueOf()] = index;
+		}
+
+		return result;
+	}
+
 	binarySearch<T, V>(arr: Indexable<T>, x: V, low: number, high: number, map: (item: T) => V, mapCompare: (value: V) => number): number {
 		if (low > high) return -1
-		
+
 		const mid = Math.floor((low + high) / 2);
 		const mappedValue = mapCompare(x);
 		const mappedComperer = mapCompare(map(arr[mid]));
@@ -73,9 +99,11 @@ export class SmokesHistoryComponent implements OnInit, AfterViewInit {
 
 	addCountBadge(dayElement: DayElement | null): void {
 		if (!dayElement) return;
-		console.log('create day', dayElement);
-		const count = this.getCountAtDay(dayElement.dateObj);
-		dayElement.innerHTML += `<span class="day-count-wrapper"><span class="day-count ${+dayElement.innerHTML < 10 ? 'correct-single-dates' : ''}">${count ?? 0}</span></span>`
+
+		const count = this.getCountAtDay(dayElement.dateObj) ?? 0;
+		const date = dayElement.dateObj.getDate();
+		const classes = `day-count${date < 10 ? ' correct-single-dates' : ''}`;
+		dayElement.innerHTML = `${date}<span class="day-count-wrapper"><span class="${classes}">${count}</span></span>`;
 	}
 
 	getCountAtDay(day: Date): number {
