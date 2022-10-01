3172242ba17a2f41879a18aa0be5c6967680c350
Amit775 <38419204+Amit775@users.noreply.github.com>
2022-10-01 03:18:55 +0300

    add edit functionality

--- src/app/features/smokes-history/smoke-record/smoke-record.component.ts
+++ src/app/features/smokes-history/smoke-record/smoke-record.component.ts
@@ -10,19 +10,43 @@ export class SmokeRecordComponent {
 
 	@Input() smoke!: ISmoke;
 	@Input() index!: number;
-	@Input() selected!: boolean;
 
-	@Output() smokeChange = new EventEmitter<ISmoke>();
+	private _selected!: boolean;
+	@Input() set selected(value: boolean) {
+		if (!value) {
+			this.isEditMode = false;
+		}
+
+		this._selected = value
+	}
+	get selected(): boolean {
+		return this._selected;
+	}
+
+	@Output() edited = new EventEmitter<ISmoke>();
 	@Output() removed = new EventEmitter<ISmoke>();
 
 	readonly time: string = "HH:mm";
 
+	isEditMode: boolean = false;
+
 	openClock(): void {
-		console.log('open clock');
+		this.isEditMode = true;
+	}
+
+	blurr(timePicker: HTMLInputElement): void {
+		console.log(timePicker, timePicker.valueAsDate, timePicker.value);
+		const [hours, minutes] = timePicker.value.split(':');
+		this.edit({ ...this.smoke, timestamp: new Date(this.smoke.timestamp).setHours(+hours, +minutes) })
+		this.isEditMode = false;
+	}
+
+	dateChanged(value: string): void {
+		console.log('change', value);
 	}
 
 	edit(smoke: ISmoke): void {
-		this.smokeChange.emit(smoke);
+		this.edited.emit(smoke);
 	}
 
 	remove(): void {
