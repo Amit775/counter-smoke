38404529e3cf7931336462987aa33ca0c6a613c8
Amit775 <38419204+Amit775@users.noreply.github.com>
2022-10-11 02:21:05 +0300

    Update ago.pipe.ts

--- src/app/features/i-smoke/ago.pipe.ts
+++ src/app/features/i-smoke/ago.pipe.ts
@@ -1,6 +1,10 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
-const dayInMs = 1000 * 60 * 60 * 24;
+const minuteInMs = 1000 * 60;
+const hourInMs = minuteInMs * 60;
+const hourInMins = hourInMs / minuteInMs;
+const dayInMs = hourInMs * 24;
+
 @Pipe({ name: 'ago', standalone: true })
 export class AgoPipe implements PipeTransform {
 	transform(value: number | undefined | null): string {
@@ -8,16 +12,12 @@ export class AgoPipe implements PipeTransform {
 
 		const currentTime = new Date();
 		const smokeTime = new Date(value);
-		if (currentTime.valueOf() - smokeTime.valueOf() > dayInMs) return 'more then a day ago';
+		const diffTime = currentTime.valueOf() - smokeTime.valueOf();
+		if (diffTime > dayInMs) return 'more then a day ago';
 
-		const [cHours, cMinutes] = [currentTime.getHours(), currentTime.getMinutes()];
-		const [sHours, sMinutes] = [smokeTime.getHours(), smokeTime.getMinutes()];
-		let diffHours = cHours - sHours;
-		let diffMins = cMinutes - sMinutes;
-		if (diffMins < 0) {
-			diffHours -= 1;
-			diffMins += 60
-		}
+		const diffInMinutes = Math.floor(diffTime / minuteInMs);
+		const diffHours = Math.floor(diffInMinutes /  hourInMins);
+		const diffMins = Math.floor(diffInMinutes % hourInMins);
 
 		if (diffHours === 0 && diffMins === 0) return 'just now';
 
