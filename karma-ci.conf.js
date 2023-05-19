031317523726397b5efb30758ba9d16fbe105230
Amit775 <38419204+Amit775@users.noreply.github.com>
2023-05-19 15:32:40 +0300

    chromeheadless test and budgets

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_INFO,
-		browsers: ['Chrome'],
+		browsers: ['ChromeHeadless'],
 		singleRun: true,
 	});
 };
