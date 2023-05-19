6437dc0ac29761c627d8ebcd044c4ca082483747
Amit775 <38419204+Amit775@users.noreply.github.com>
2023-05-19 14:49:29 +0300

    format fix

--- src/app/features/smokes-history/smoke-record/smoke-record.component.ts
+++ src/app/features/smokes-history/smoke-record/smoke-record.component.ts
@@ -1,55 +1,1 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { ISmoke } from 'src/app/core/smokes/smokes.store';
-
-@Component({
-	selector: 'app-smoke-record',
-	templateUrl: './smoke-record.component.html',
-	styleUrls: ['./smoke-record.component.scss'],
-	// changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class SmokeRecordComponent {
-
-	@Input() smoke!: ISmoke;
-	@Input() index!: number;
-
-	private _selected!: boolean;
-	@Input() set selected(value: boolean) {
-		if (!value) {
-			this.isEditMode = false;
-		}
-
-		this._selected = value
-	}
-	get selected(): boolean {
-		return this._selected;
-	}
-
-	@Output() edited = new EventEmitter<ISmoke>();
-	@Output() removed = new EventEmitter<ISmoke>();
-
-	readonly time: string = "HH:mm";
-
-	isEditMode: boolean = false;
-
-	openClock(): void {
-		this.isEditMode = true;
-	}
-
-	changeTime(timePicker: HTMLInputElement): void {
-		const [hours, minutes] = timePicker.value.split(':');
-		this.edit({ ...this.smoke, timestamp: new Date(this.smoke.timestamp).setHours(+hours, +minutes) })
-		this.isEditMode = false;
-	}
-
-	cancelEdit(): void {
-		this.isEditMode = false;
-	}
-
-	edit(smoke: ISmoke): void {
-		this.edited.emit(smoke);
-	}
-
-	remove(): void {
-		this.removed.emit(this.smoke);
-	}
-}
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';import { ISmoke } from 'src/app/core/smokes/smokes.store';@Component({	selector: 'app-smoke-record',	templateUrl: './smoke-record.component.html',	styleUrls: ['./smoke-record.component.scss'],	// changeDetection: ChangeDetectionStrategy.OnPush})export class SmokeRecordComponent {	@Input() smoke!: ISmoke;	@Input() index!: number;	private _selected!: boolean;	@Input() set selected(value: boolean) {		if (!value) {			this.isEditMode = false;		}		this._selected = value;	}	get selected(): boolean {		return this._selected;	}	@Output() edited = new EventEmitter<ISmoke>();	@Output() removed = new EventEmitter<ISmoke>();	readonly time: string = 'HH:mm';	isEditMode: boolean = false;	openClock(): void {		this.isEditMode = true;	}	changeTime(timePicker: HTMLInputElement): void {		const [hours, minutes] = timePicker.value.split(':');		this.edit({ ...this.smoke, timestamp: new Date(this.smoke.timestamp).setHours(+hours, +minutes) });		this.isEditMode = false;	}	cancelEdit(): void {		this.isEditMode = false;	}	edit(smoke: ISmoke): void {		this.edited.emit(smoke);	}	remove(): void {		this.removed.emit(this.smoke);	}}
